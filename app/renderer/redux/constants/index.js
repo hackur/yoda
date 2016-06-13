a4830f0ca9bc48b62b45b3f1935f7b8528021eb3
Bhargav Anand <rjn143@gmail.com>
2016-06-13 05:11:21 -0700

    Separate app constants to its own module

---
+++ app/renderer/redux/constants/index.js
@@ -0,0 +1,11 @@
+export default {
+  channels: {
+    popular: 'UCF0pVplsI8R5kcAqgtoRqoA',
+    music: 'UC-9-kyTW8ZkZNDHQJ6FgpwQ',
+    sports: 'UCEgdi0XIXXZ-qJOFPf4JSKw',
+    gaming: 'UCOpNcN46UbXVtpKMrmU4Abg',
+    education: 'UC3yA8nDwraeOfnYfBWun83g',
+    news: 'UCYfdidRxbB8Qhf0Nx7ioOYw',
+    spotlight: 'UCBR8-60-B28hp2BmDPdntcQ'
+  }
+}
