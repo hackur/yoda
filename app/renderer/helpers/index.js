3feec9534e2473773e86eec1237faab27b60371f
Bhargav Anand <rjn143@gmail.com>
2016-06-20 08:33:41 -0700

    add Provider HOC

--- app/renderer/helpers/index.js
+++ app/renderer/helpers/index.js
@@ -1,3 +1,5 @@
+import React, { Component, PropTypes } from 'react'
+
 export function uuid():string {
   const _s4 = () => Math.floor((1+Math.random())*0x10000).toString(16).substr(1)
   return `${_s4()+_s4()}-${_s4()}-${_s4()}-${_s4()+_s4()+_s4()}`
@@ -7,9 +9,21 @@ export function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
-// export function getSpringConfig(stiffness, damping) {
-//   return {
-//     stiffness: stiffness,
-//     damping: damping
-//   }
-// }
+export class Provider extends Component {
+  static childContextTypes = {
+    store: PropTypes.object
+  }
+
+  constructor(props, context) {
+    super(props, context);
+    this.store = props.store
+  }
+
+  getChildContext() {
+    return { store: this.store };
+  }
+
+  render() {
+    return React.Children.only(this.props.children);
+  }
+}
